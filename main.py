3900eb1dc1eb39109a33efb99816f4d736f73b18
Moura Yuri <mourayuri339@gmail.com>
2020-10-15 23:32:33 -0300

    blob detection implemented

--- main.py
+++ main.py
@@ -9,10 +9,29 @@ ret,thresh1 = cv2.threshold(blurryImage,140,255,cv2.THRESH_BINARY)
 kernel = np.ones((10,10),np.uint8)
 erosion = cv2.erode(thresh1,kernel,iterations = 1)
 
-detector = cv2.SimpleBlobDetector()
-detector.detect(thresh1)
+params = cv2.SimpleBlobDetector_Params()
 
-#print(len(keypoints))
+# Change thresholds
+params.minThreshold = 0
+params.maxThreshold = 255
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 1
+params.maxArea = 1000000
+
+params.filterByCircularity = False
+
+params.filterByColor = False
+
+params.filterByInertia = False
+
+params.filterByConvexity = False
+
+detector = cv2.SimpleBlobDetector_create(params)
+keypoints = detector.detect(thresh1)
+
+print(len(keypoints))
 #cv2.imshow("erosion",erosion)
-cv2.waitKey(0)
+#cv2.waitKey(0)
 cv2.destroyAllWindows()
